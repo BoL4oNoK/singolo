6e883f8c6b66b3dc0637709554064dbd52c2308c
Maksim Antonov <33826709+BoL4oNoK@users.noreply.github.com>
2020-03-12 13:09:41 +0300

    fix: handlers

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const PORTFOLIO = document.getElementById('portfolio-list');
 const portfolioList = PORTFOLIO.querySelectorAll('div');
 
 function menuhandler(event) {
-    MENU.querySelectorAll('li').forEach(el => {
-        el.classList.remove('menu_active');
-    });
-    const elem = event.target.parentNode;
-    elem.classList.add('menu_active');
+    if (event.target.tagName === 'A') {
+        MENU.querySelectorAll('li').forEach(el => {
+            el.classList.remove('menu_active');
+        });
+        const elem = event.target.parentNode;
+        elem.classList.add('menu_active');
+    }
 }
 
 function buttonhandler(event) {
-    BUTTONS.querySelectorAll('button').forEach(el => {
-        el.classList.remove('button_active');
-    });
-    
-    PORTFOLIO.insertAdjacentElement('afterbegin', portfolioList[portfolioList.length - 1]);
-    const elem = event.target;
-    elem.classList.add('button_active');
+    if (event.target.tagName === 'BUTTON') {
+        BUTTONS.querySelectorAll('button').forEach(el => {
+            el.classList.remove('button_active');
+        });
+        
+        PORTFOLIO.insertAdjacentElement('afterbegin', portfolioList[portfolioList.length - 1]);
+        const elem = event.target;
+        elem.classList.add('button_active');
+    }
 }
 
 MENU.addEventListener('click', menuhandler);
